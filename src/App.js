9d6d1fcaf61057f5c5741bab15eaadd0a3173e6e
Jose Diestra <jdiestra@Joses-MacBook-Pro.local>
2022-06-02 17:35:13 -0500

    feat: changes

--- src/App.js
+++ src/App.js
@@ -1,90 +1,97 @@
 import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { Reorder, getItemStyle, getQuestionListStyle } from "./utils";
+import Answers from "./answer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './App.css';
 
-const finalSpaceCharacters = [
-  {
-    id: 'element-1',
-    name: 'Gary Goodspeed',
-    thumb: '/images/gary.png'
-  },
-  {
-    id: 'element-2',
-    name: 'Little Cato',
-    thumb: '/images/cato.png'
-  },
-  {
-    id: 'element-3',
-    name: 'KVN',
-    thumb: '/images/kvn.png'
-  },
-  {
-    id: 'element-4',
-    name: 'Mooncake',
-    thumb: '/images/mooncake.png'
-  },
-  {
-    id: 'element-5',
-    name: 'Quinn Ergon',
-    thumb: '/images/quinn.png'
-  }
-]
+
+const getQuestions = (count) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `question-${k}`,
+    content: `question ${k}`,
+    answers: [`answer-1`, `answer-2`, `answer-3`]
+  }));
+
+  
 
 function App() {
-  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+  const [questions, setQuestions ] = useState(getQuestions(5));
 
   function handleOnDragEnd(result) {
-    if (!result.destination) return;
+    if (!result.destination) {
+      return;
+    }
 
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    if (result.type === "QUESTIONS") {
+      console.log(result);
+      const questions = Reorder(
+        this.state.questions,
+        result.source.index,
+        result.destination.index
+      );
 
-    updateCharacters(items);
+      setQuestions(questions);
+    } else {
+      console.log(result);
+      const answers = Reorder(
+        this.state.questions[parseInt(result.type, 10)].answers,
+        result.source.index,
+        result.destination.index
+      );
+
+      const questions = JSON.parse(JSON.stringify(this.state.questions));
+
+      questions[result.type].answers = answers;
+
+      setQuestions(questions);
+    }
   }
 
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Final Space Characters</h1>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="characters" direction="vertical" type="QUESTION">
-            {(provided) => (
-              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                {characters.map((character, index) => {
-                  console.log(index)
-                  return (
-                    <Draggable key={character.id} draggableId={character.id} index={index} type="TASK">
-                      {(provided) => (
-                        <li ref={provided.innerRef} {...provided.draggableProps} >
-                          <span {...provided.dragHandleProps}>
-                            <FontAwesomeIcon
-                              icon={"grip-vertical"}
-                              style={{ float: "left" }}
-                            />
-                          </span>
-                          <div className="characters-thumb">
-                            <img src={character.thumb} alt={`${character.name} Thumb`} />
-                          </div>
-                          <p>
-                            { character.name }
-                          </p>
-                        </li>
+    <DragDropContext
+        onDragEnd={handleOnDragEnd}
+      >
+        <Droppable droppableId="droppable" type="QUESTIONS">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getQuestionListStyle(snapshot.isDraggingOver)}
+            >
+              {questions.map((question, index) => (
+                <Draggable
+                  key={question.id}
+                  draggableId={question.id}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
                       )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </header>
-      <p>
-        Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
-      </p>
-    </div>
+                    >
+                      {question.content}
+                      <span {...provided.dragHandleProps}>
+                        <FontAwesomeIcon
+                          icon={"grip-vertical"}
+                          style={{ float: "left" }}
+                        />
+                      </span>
+                      <Answers questionNum={index} question={question} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ import { faGripVertical } from "@fortawesome/free-solid-svg-icons";
 library.add(faGripVertical);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
